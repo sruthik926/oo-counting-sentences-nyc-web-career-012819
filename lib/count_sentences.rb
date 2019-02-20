97dff3a7dc32787555009104e284c4aac5a81db1
github username <github email address>
2019-02-20 11:04:41 -0500

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+       true
+    else
+       false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+       true
+    else
+       false
+    end
   end
 
   def exclamation?
+    if self.end_with?("!")
+       true
+    else
+       false
+    end
 
   end
 
-  def count_sentences
 
+def count_sentences
+
+  countSentences = 0
+  puts sentenArr = self.split(/?|\...|\,)
+  #binding.pry
+  sentenArr.each do |sentence|
+     countSentences += 1
   end
+     countSentences
+      #binding.pry
+end
+
 end
